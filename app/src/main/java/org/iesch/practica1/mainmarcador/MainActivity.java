d610c0349d4e9e926d45dc161f48ce90246d3fae
michel <mrubios@iesch.org>
2022-10-05 23:20:54 +0200

    Diseño de la primera pantalla excepto el icono de un botón

---
+++ app/src/main/java/org/iesch/practica1/mainmarcador/MainActivity.java
@@ -0,0 +1,13 @@
+package org.iesch.practica1.mainmarcador;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
