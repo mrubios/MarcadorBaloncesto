aa48aa5210f6f60a1fc76968fb37f7a43d1dbd41
michel <mrubios@iesch.org>
2022-10-05 23:47:26 +0200

    Segunda ventana diseñada (nada de código en los .java)

---
+++ app/src/main/java/org/iesch/practica1/mainmarcador/ResultadoBasket.java
@@ -0,0 +1,14 @@
+package org.iesch.practica1.mainmarcador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultadoBasket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado_basket);
+    }
+}
