ccba81f79ef73b06590dfb17c793d54071d73650
dam2 <mrubios>
2022-10-06 17:37:07 +0200

    Terminado pero no cambia de ventana

--- app/src/main/java/org/iesch/practica1/mainmarcador/ResultadoBasket.java
+++ app/src/main/java/org/iesch/practica1/mainmarcador/ResultadoBasket.java
@@ -4,11 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.iesch.practica1.mainmarcador.databinding.ActivityResultadoBasketBinding;
+
 public class ResultadoBasket extends AppCompatActivity {
+
+    private ActivityResultadoBasketBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_resultado_basket);
+        setContentView(binding.getRoot());
+        int localScore = getIntent().getExtras().getInt("localScore");
+        int visitorScore = getIntent().getExtras().getInt("visitorScore");
+
+        binding.lblMarcador.setText(String.valueOf(localScore)+ " - " + String.valueOf(visitorScore));
+
+        if (localScore>visitorScore){
+            binding.lblResultado.setText("Ganó el equipo local");
+        }
+        else  if (visitorScore>localScore)
+        {
+            binding.lblResultado.setText("Ganó el equipo visitante");
+        }
+        else{
+            binding.lblResultado.setText("Empate");
+
+        }
+
+
     }
 }
